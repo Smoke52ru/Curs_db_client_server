871f6880eb5c8efcaf5e836c491b0bac28ed3ab2
Smoke <Lapin-Artem@ya.ru>
2021-04-20 00:53:02 +0300

    numhandler

--- handler.h
+++ handler.h
@@ -17,11 +17,28 @@ int isseparator(char* ch, char* separators){
     return 0;
 }
 
+int numhandler(char* word, int* sizep, char* wordname){
+    char* ptr = word;
+    int i = 0;
+    while ((*ptr != ' ') && (*ptr != '\0')) {
+        if (isdig(*ptr)){ //  Цифра
+            ptr++;
+            i++;
+        } else {
+            fprintf(stderr, "ERROR: Invalid [%s]\n", wordname);
+            return 1;
+        }
+    }
+    *sizep = i;
+    fprintf(stdout, "[%s] is correct\n", wordname);
+    return 0;
+}
+
 int wordhandler(char* word, int* sizep, char* wordname){
     char* ptr = word;
     int i = 0;
     while ((*ptr != ' ') && (*ptr != '\0')) {
-        if (iseng(*ptr) || isdig(*ptr)){ //  Буква или цифра
+        if (iseng(*ptr)){ //  Буква
             ptr++;
             i++;
         } else {
@@ -35,7 +52,7 @@ int wordhandler(char* word, int* sizep, char* wordname){
 }
 
 
-
+// Прототип (TODO поддержка команд)
 int handler(char *data) {
 
     char *str[5] = {"OP", "RD", "WR", "AD", "DL"}; // Набор команд
@@ -44,33 +61,19 @@ int handler(char *data) {
     for (cmd_num = 0; cmd_num < 5; cmd_num++) {
         if (strncmp(data, str[cmd_num], 2) == 0) {
             cur_char = 3;
-            while (data[cur_char] != ' ') {
-                if ((data[cur_char]<'0') || (data[cur_char]>'9')) {
-                    fprintf(stderr,"ERROR: Invalid character in [Index]\n");
-                    return 1;
-                }
-                cur_char++;
-            }
-            fprintf(stdout, "[Index] is correct\n");
-            cur_char++;
-
-            if(wordhandler(&(data[cur_char]), &size, "Name")){
+            if (numhandler(&data[cur_char], &size, "Index")){
                 return 1;
             }
             cur_char += size+1;
-            /*while (data[cur_char] != ' ') {
-                cur_char++;
-            }
-            fprintf(stdout, "[Name] is correct\n");
-            cur_char++;
-            */
 
-
-
-            if(wordhandler(&(data[cur_char]), &size, "Surname")){
+            if(wordhandler(&data[cur_char], &size, "Name")){
                 return 1;
             }
-            //fprintf(stdout, "[Surname] is correct\n");
+            cur_char += size+1;
+
+            if(wordhandler(&data[cur_char], &size, "Surname")){
+                return 1;
+            }
             return 0;
         } else {
             continue;
